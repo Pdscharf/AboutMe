1061d0f19384cbe124f4c7381f81505db2d1975f
Pdscharf <77867405+Pdscharf@users.noreply.github.com>
2021-03-01 22:52:46 -0500

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by Paul Scharf'));
-});
