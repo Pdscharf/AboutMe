1c360ea48f81b7e60eaac7965efcf307dd9ff55f
Pdscharf <77867405+Pdscharf@users.noreply.github.com>
2021-03-01 22:47:45 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  var ulElement = document.getElementById('links');
+  var liElement = document.createElement('li');
+  liElement.appendChild(document.createTextNode('created by Paul Scharf'));
+});
